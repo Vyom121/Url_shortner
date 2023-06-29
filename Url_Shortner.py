1dc44325c6430da1a42db0831943e6122d2b1f51
Vyom Singh <96306267+Vyom121@users.noreply.github.com>
2023-06-29 22:13:22 +0530

    Update Url_Shortner.py
    Fixed size of the window 
    Made Appereance Improvements in Buttons

--- Url_Shortner.py
+++ Url_Shortner.py
@@ -1,47 +1,50 @@
 import pyshorteners
 import tkinter as tk
+import tkinter.messagebox as messagebox
+import tkinter.ttk as ttk
 
 def shorten_url():
-    # Retrieve the long URL from the Entry widget
     long_url = entry_url.get()
-
-    # Initialize the Shortener object
     s = pyshorteners.Shortener()
-
-    # Shorten the URL
     shortened_url = s.tinyurl.short(long_url)
-
-    # Update the result label with the shortened URL
     result_label.config(text=shortened_url)
 
-# Copy the shortened URL to the clipboard
 def copy_to_clipboard():
-    window.clipboard_clear()  # Clear clipboard contents
-    # Copy to clipboard
+    window.clipboard_clear()
     window.clipboard_append(result_label.cget("text"))
     messagebox.showinfo("Copied!", "The shortened URL has been copied to the clipboard.")
 
-
-
-# Create the main window
 window = tk.Tk()
-window.title("URL Shortener")
+window.title("URL Shortener Application")
+window.resizable(False, False)
+window.geometry("500x150")
 
-# Create an Entry widget to enter the long URL
-entry_url = tk.Entry(window, width=50)
-entry_url.pack(pady=10)
+label_instructions = tk.Label(window, text="Enter a long URL to shorten:")
+label_instructions.pack(pady=5)
 
-# Create a button to shorten the URL
-button_shorten = tk.Button(window, text="Shorten", command=shorten_url)
-button_shorten.pack()
+frame_input = tk.Frame(window)
+frame_input.pack(pady=5)
 
-# Create a label to display the result
-result_label = tk.Label(window, text="")
-result_label.pack(pady=10)
+entry_url = ttk.Entry(frame_input, width=50)
+entry_url.pack(side=tk.LEFT)
 
-# Button to copy the url
-button_copy = tk.Button(window, text="Copy", command=copy_to_clipboard)
-button_copy.pack()
+button_shorten = ttk.Button(frame_input, text="Shorten", command=shorten_url)
+button_shorten.pack(side=tk.LEFT, padx=5)
 
-# Start the GUI main loop
+frame_result = tk.Frame(window)
+frame_result.pack(pady=5)
+
+result_label = ttk.Label(frame_result, text="")
+result_label.pack(side=tk.LEFT)
+
+button_copy = ttk.Button(frame_result, text="Copy", command=copy_to_clipboard)
+button_copy.pack(side=tk.LEFT, padx=5)
+
+style = ttk.Style()
+style.configure("TButton", padding=6)
+style.configure("TLabel", padding=4)
+style.configure("TEntry", padding=4)
+style.configure("TFrame", padding=6)
+
+window.eval('tk::PlaceWindow %s center' % window.winfo_toplevel())
 window.mainloop()
